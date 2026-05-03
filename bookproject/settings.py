ce32a5f922a50a49f4ad9646a162ba728d8a21ba
techgym <support@techgym.jp>
2026-05-03 15:19:05 +0900

    STATIC_ROOT設定を追加

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/image/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
